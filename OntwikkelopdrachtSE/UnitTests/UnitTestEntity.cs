1865765efe9aaeaec702e901cca9b1812c9bbb6e
Teun <teunvanlaarhoven@gmail.com>
2016-09-23 09:52:18 +0200

    Added all (blank) unit tests

---
+++ OntwikkelopdrachtSE/UnitTests/UnitTestEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestEntity
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
