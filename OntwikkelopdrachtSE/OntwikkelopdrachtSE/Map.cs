0a15e1460fc5461332d3b5750da0d16d4533000b
tvl20 <teunvanlaarhoven@gmail.com>
2016-10-03 18:39:37 +0200

    Added additional options for the txt file.

--- OntwikkelopdrachtSE/OntwikkelopdrachtSE/Map.cs
+++ OntwikkelopdrachtSE/OntwikkelopdrachtSE/Map.cs
@@ -17,16 +17,32 @@ namespace OntwikkelopdrachtSE
         {
             Tiles = new List<Tile>();
             EnemyDefault = new List<Enemy>();
-            //in the file everything has to go on ONE line, otherwise you get an error!!
+            //in the file everything has to go on ONE line, otherwise you'll get an error!!
             string text = System.IO.File.ReadAllText(path);
             int counter = 0;
             for (int y = 1; y < 31; y++)
             {
                 for (int x = 1; x < 31; x++)
                 {
-                    if(text.Substring(counter, 1) == "W")
+                    if (text.Substring(counter, 1) == "X")
+                    {
+                        Tiles.Add(new Tile(x, y, Tile.type.Blank));
+                    }
+                    else if (text.Substring(counter, 1) == "E")
+                    {
+                        Tiles.Add(new Tile(x, y, Tile.type.Enemy));
+                    }
+                    else if (text.Substring(counter, 1) == "P")
+                    {
+                        Tiles.Add(new Tile(x, y, Tile.type.Player));
+                    }
+                    else if (text.Substring(counter, 1) == "W")
                     {
                         Tiles.Add(new Tile(x, y, Tile.type.Wall));
+                    }
+                    else if (text.Substring(counter,1) == "T")
+                    {
+                        Tiles.Add(new Tile(x, y, Tile.type.Trap));
                     }
                     counter++;
                 }
